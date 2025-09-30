e58001d46f9e35d5af568469aa1939621003de14
Calvin Ronksley <ronksleycalvin0@gmail.com>
2025-10-01 00:18:26 +0200

    added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyperxar",
-    version="0.2.0",
+    version="0.3.1",
     author="Calvin Ronksley",
     author_email="youremail@example.com",
     description="Python3 Modul zum Kopieren, Komprimieren, Extrahieren und ISO-Erstellen mit Fortschrittsanzeige",
