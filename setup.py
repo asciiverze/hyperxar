b6fdcb6a473ce477377d0c030b25e484776c6f09
Calvin Ronksley <ronksleycalvin0@gmail.com>
2025-09-30 22:07:51 +0200

    Initial commit hyperxar

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="hyperxar",
-    version="0.1.0",
+    name="hyperxar",  # Name des Moduls auf PyPI
+    version="0.2.0",  # Versionsnummer
     author="Calvin Ronksley",
-    author_email="youremail@example.com",
-    description="Ein Python3 Modul zum Kopieren, Komprimieren und ISO erstellen mit Fortschrittsanzeige.",
+    author_email="ronksleycalvin0@gmail.com",
+    description="Python3 Modul zum Kopieren, Komprimieren, Extrahieren und ISO-Erstellen mit Fortschrittsanzeige",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/deinusername/hyperxar",
-    packages=find_packages(),
+    url="https://github.com/MultiCodexz/hyperxar",  # Link zu GitHub
+    packages=find_packages(),  # findet automatisch das Paket hyperxar
     install_requires=[
         "tqdm",
         "py7zr"
